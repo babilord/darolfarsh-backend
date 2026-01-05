6df8ce47e0d4bb5a324722fe485a688def47704c
keyvan.javanmiri@yahoo.com <keyvan.javanmiri@yahoo.com>
2026-01-05 15:47:41 +0330

    --

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from knox import views as knox_views
 from django.views.decorators.csrf import csrf_exempt
+from .views import KnoxLoginAPI
 from .views import (
     health,
     register,
@@ -17,20 +18,21 @@ urlpatterns = [
     path("register/", csrf_exempt(register)),
 
     # Knox login/logout
+    #path("login/", KnoxLoginAPI.as_view(), name="knox_login"),
     path("login/", csrf_exempt(knox_views.LoginView.as_view()), name="knox_login"),
     path("logout/", csrf_exempt(knox_views.LogoutView.as_view()), name="knox_logout"),
     path("logoutall/", csrf_exempt(knox_views.LogoutAllView.as_view()), name="knox_logoutall"),
 
     # current user
-    path("me/", csrf_exempt(me)),
+    path("me/", me),
 
     # update profile
-    path("profile/", csrf_exempt(update_profile)),  # PATCH /accounts/api/profile/
+    path("profile/", update_profile),  # PATCH /accounts/api/profile/
 
     # debug timezone
-    path("dbtz/", csrf_exempt(dbtz)),
+    path("dbtz/", dbtz),
 
     # forgot/reset password
-    path("forgot-password/", csrf_exempt(forgot_password)),
-    path("reset-password/", csrf_exempt(reset_password)),
+    path("forgot-password/", forgot_password),
+    path("reset-password/", reset_password),
 ]
