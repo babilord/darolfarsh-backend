589fe377564990238b1c99c5267a8e30c0b8f0c2
keyvan.javanmiri@yahoo.com <keyvan.javanmiri@yahoo.com>
2026-01-06 21:25:52 +0330

    --fiix 8

--- ghalichin/settings.py
+++ ghalichin/settings.py
@@ -166,14 +166,17 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
 }
 
+
+#CORS_ALLOW_ALL_ORIGINS = True
+#CORS_ALLOW_CREDENTIALS = True
+
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "https://daralfarsha.com",
     "daralfarsha.com",
+    "*",
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOW_CREDENTIALS = True
 
 
 # CORS_ORIGIN_WHITELIST = [
