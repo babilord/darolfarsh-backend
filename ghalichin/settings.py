726be6818a7e8d42d84ec53b7908a8be094b7b27
keyvan.javanmiri@yahoo.com <keyvan.javanmiri@yahoo.com>
2026-01-07 13:56:52 +0330

    add new cors to setting

--- ghalichin/settings.py
+++ ghalichin/settings.py
@@ -171,6 +171,8 @@ CORS_ORIGIN_WHITELIST = [
     "http://localhost:3000",
     "https://daralfarsha.com",
     "http://127.0.0.1:3000",
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
 ]
 
 # CORS_ALLOW_METHODS = [
